08c197f96bc71c0850d37ac73a6760d4844af08b
Les Pounder <lespounder@gmail.com>
2014-10-29 21:12:21 +0000

    updated code to control 6 groups of LED

--- project.py
+++ project.py
@@ -20,7 +20,7 @@ import time
 import datetime
 import RPi.GPIO as GPIO
 import pygame
-import time
+#import time
 import picamera
 from random import choice
 
@@ -37,8 +37,10 @@ global distance
 
 LED1 = 8
 LED2 = 10
-#LED3 = 12
-#LED4 = 16
+LED3 = 12
+LED4 = 16
+LED5 = 18
+LED6 = 22
 
 red = 36
 blue = 38
@@ -57,8 +59,10 @@ song3 = "./merry.mp3"
 
 GPIO.setup(LED1, GPIO.OUT)
 GPIO.setup(LED2, GPIO.OUT)
-#GPIO.setup(LED3, GPIO.OUT)
-#GPIO.setup(LED4, GPIO.OUT)
+GPIO.setup(LED3, GPIO.OUT)
+GPIO.setup(LED4, GPIO.OUT)
+GPIO.setup(LED5, GPIO.OUT)
+GPIO.setup(LED6, GPIO.OUT)
 GPIO.setup(trigger, GPIO.OUT)
 
 GPIO.setup(red, GPIO.OUT)
@@ -67,10 +71,6 @@ GPIO.setup(blue, GPIO.OUT)
 
 GPIO.setup(echo, GPIO.IN)
 
-#Create List which we will use to play a *random* song
-
-music = ["song1","song2","song3"]
-
 #Define Functions
 
 def ultra(sensor):
@@ -78,7 +78,8 @@ def ultra(sensor):
     if sensor == 0:
 
         time.sleep(0.3)
-
+        signaloff = 0
+        signalon = 0
         GPIO.output(trigger, True)
 
         time.sleep(0.00001)
@@ -101,17 +102,21 @@ def music(x):
 	pygame.mixer.music.load(x)
 	pygame.mixer.music.play(1)
 
-def flash(a,b):
+def flash(a,b,c,d,e,f):
 		GPIO.output(a, True)
 		GPIO.output(b, False)
-		#GPIO.output(c, True)
-		#GPIO.output(d, False)
+		GPIO.output(c, True)
+		GPIO.output(d, False)
+		GPIO.output(e, True)
+		GPIO.output(f, False)		
 		time.sleep(0.2)
 		GPIO.output(a, False)
 		GPIO.output(b, True)
-		#GPIO.output(c, False)
-		#GPIO.output(d, True)
-		#time.sleep(0.2)
+		GPIO.output(c, False)
+		GPIO.output(d, True)
+		GPIO.output(e, False)
+		GPIO.output(f, True)
+		time.sleep(0.2)
 
 def rgb():
 	GPIO.output(red, True)
@@ -142,7 +147,7 @@ def reset(red,green,blue,a,b):
 
 #Cue to tell me that the system is ready
 music("./sleigh.mp3")
-reset(red,green,blue,LED1,LED2)
+reset(red,green,blue,LED1,LED2,LED3,LED4)
 
 
 while True:
@@ -160,9 +165,10 @@ while True:
 		camera.capture(pic)
 		music(chosen)
 		for i in range(100):
-			flash(LED1,LED2)
+			flash(LED1,LED2,LED3,LED4,LED5,LED6)
 			rgb()
 
 		time.sleep(1)
 	else:
 		print("Waiting for Santa")
+		time.sleep(1)
